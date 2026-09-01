40848885fce22a22bcaaabc943c961ffd538dc90
Hrudaya Sagar Neeluri <hrudayasagar123@gmail.com>
2026-09-02 00:19:08 +0530

    Time: 48 ms (43.06%) | Memory: 19.3 MB (8.78%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        INT_MIN, INT_MAX = -2147483648, 2147483647
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        
+        rev = 0
+        while x != 0:
+            digit = x % 10
+            x //= 10
+
+            if rev > INT_MAX // 10 or (rev == INT_MAX // 10 and digit > 7):
+                return 0
+                
+            rev = rev * 10 + digit
+            
+        return rev * sign
