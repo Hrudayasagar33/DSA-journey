a07bf1ff3a34db9c9663a0e3a6cbb0ca3789e48f
Hrudaya Sagar Neeluri <hrudayasagar123@gmail.com>
2026-09-13 15:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (32.04%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        strs.sort()
+        
+        first = strs[0]
+        last = strs[-1]
+        
+        prefix = []
+        for i in range(min(len(first), len(last))):
+            if first[i] == last[i]:
+                prefix.append(first[i])
+            else:
+                break
+                
+        return "".join(prefix)
